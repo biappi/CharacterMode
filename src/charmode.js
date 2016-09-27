acee802770036ad6071910551b5a385f76113df0
Antonio Willy Malara <antonio.malara@gmail.com>
2016-09-28 00:58:50 +0200

    implementing drag painting

--- src/charmode.js
+++ src/charmode.js
@@ -8,7 +8,7 @@ function character_pixel(chargen, charidx, x, y) {
 function create_table(id, w, h, content) {
     content = content || function() { return "<div></div>" }
 
-    var html = '<table id="' + id + '" class="pixel" onclick="tile_on_click(this)">'
+    var html = '<table id="' + id + '" class="pixel" onmousedown="tile_on_down(this)" onmouseup="tile_on_up(this)" onmousemove="tile_on_move(this)">'
     for (var y = 0; y < h; y++) {
         html += '<tr>'
         for (var x = 0; x < w; x++) {
@@ -49,8 +49,13 @@ function draw_screen(screen_id, chargen, content) {
     }
 }
 
-var all_callbacks = {}
-function tile_on_click(element) {
+var all_callbacks = {
+    down: {},
+    move: {},
+    up:   {},
+}
+
+function tile_do_cb(element, type) {
     var matches = /(\w+)-(\d+)-(\d+)/.exec(element.id)
     if (!matches)
         return
@@ -59,22 +64,20 @@ function tile_on_click(element) {
     var x = parseInt(matches[2])
     var y = parseInt(matches[3])
 
-    var cb = all_callbacks[thing]
+    var cb = all_callbacks[type][thing]
     if (cb)
         cb(x, y)
 }
 
-function main() {
-    var screen_content = []
-    for (var x = 0; x < 40; x++) {
-        screen_content[x] = []
-        for (var y = 0; y < 25; y++) {
-            screen_content[x][y] = 0
-        }
-    }
+function tile_on_down(el) { tile_do_cb(el, 'down') }
+function tile_on_move(el) { tile_do_cb(el, 'move') }
+function tile_on_up(el)   { tile_do_cb(el, 'up')   }
 
+function main() {
     var char_size =    { w:  8, h: 8 }
     var palette_size = { w: 16, h: (32 / 2) }
+
+    /* Creating elements */
 
     document.write("<table><tr><td>")
 
@@ -91,6 +94,16 @@ function main() {
     document.write("<br><br>Selected char: ")
     document.write(create_table('selected-char', char_size.w, char_size.h))
     document.write("</td></tr></table>")
+
+    /* Initialization */
+
+    var screen_content = []
+    for (var x = 0; x < 40; x++) {
+        screen_content[x] = []
+        for (var y = 0; y < 25; y++) {
+            screen_content[x][y] = 0
+        }
+    }
 
     var c = 0
     for (var y = 0; y < palette_size.h; y++) {
@@ -109,13 +122,48 @@ function main() {
 
     set_selected(0)
 
-    all_callbacks['palette'] = function(x, y) {
+    var paint_screen = function(x, y) {
+        screen_content[x][y] = selected_char
+        draw_char(screenpos_cssid("screen", x, y), chargen, screen_content[x][y])
+    }
+
+    /* Callbacks for character palette */
+
+    all_callbacks['up']['palette'] = function(x, y) {
         set_selected(y * 16 + x)
     }
 
-    all_callbacks['screen'] = function(x, y) {
-        screen_content[x][y] = selected_char
-        draw_char(screenpos_cssid("screen", x, y), chargen, screen_content[x][y])
+    /* Callbacks for screen area */
+
+    var screen_down_x = -1
+    var screen_down_y = -1
+
+    all_callbacks['down']['screen'] = function(x, y) {
+        screen_down_x = x
+        screen_down_y = y
+
+        paint_screen(x, y)
+    }
+
+    all_callbacks['move']['screen'] = function(x, y) {
+        if (screen_down_x == -1 && screen_down_y == -1)
+            return
+
+        if (screen_down_x == x && screen_down_y == y)
+            return
+
+        screen_down_x = x
+        screen_down_y = y
+
+        paint_screen(x, y)
+    }
+
+    all_callbacks['up']['screen'] = function(x, y) {
+        screen_down_x = -1
+        screen_down_y = -1
+
+        if (screen_down_x != x || screen_down_y != y)
+            paint_screen(x, y)
     }
 }
 
